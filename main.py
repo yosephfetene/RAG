76f2261c20e0330acef1a34f632e5f4029aec6e1
yosephfetene <155078686+yosephfetene@users.noreply.github.com>
2025-11-14 10:05:40 +0300

    Extraction Done.

--- main.py
+++ main.py
@@ -1,35 +1,41 @@
 import os
-from pathlib import Path
-import pdfplumber
-def main():
-    inputDir = Path("pdf/Biology")
-    outputDir = Path("Output")
+import pdf2image
+import pytesseract
 
-    files = []
+def main():
+    inputDir = './pdf/Biology'
+    outputDir = './Output'
     try:
         files = os.listdir(inputDir)
-    except Exception as e:
-        print("Error accessing directory:", e)
+    except Exception as error:
+        print(f"fs.readdir didnt work and error reading directory {inputDir}", error)
         return
-    
-    for file in files:
-        pdfPath = inputDir / file
-        baseName = os.path.splitext(file)[0]
 
-        print(f"processing file: {pdfPath}")
-        
+    if not files:
+        print(f"Files in {inputDir} can't be found")
+        return
+
+    for file in files:
+        pdfPath = os.path.join(inputDir, file)
+        baseName = os.path.splitext(file)[0]
+        print(f"processing {file}")
+
         finalText = ""
-        try: 
-            with pdfplumber.open(pdfPath) as pdf:
-                for i, page in enumerate(pdf.pages)
-                text = page.extract_text() or ""
-                finalText += f"\n--- Page: {i + 1}---\n"
+
+        try:
+            pages = pdf2image.convert_from_path(pdfPath, dpi=300)
+            for i, page in enumerate(pages):
+                text = pytesseract.image_to_string(page, lang='eng')
+                finalText += f"\n--- Page: {i + 1} ---\n{text}\n"
         except Exception as err:
-            print(f"Failed to extracct text from {file}:", err)
+            print(f"Failed to extract {file}:", err)
             continue
+
         outPath = os.path.join(outputDir, baseName + ".txt")
         with open(outPath, "w", encoding="utf-8") as f:
             f.write(finalText.strip())
-        print(f"Saved and extracted text to: {outPath}")
+
+        print(f"Saved OCR result to: {outPath}")
+
 if __name__ == "__main__":
     main()
